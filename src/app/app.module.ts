892cb4dc061e915524edbc32242b9592570bd527
Graig Janssen <graig.janssen@gmail.com>
2018-09-12 19:54:38 -0400

    Initial setup, generate characters, detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { DetailComponent } from './characters/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    DetailComponent,
   ],
   imports: [
     BrowserModule
