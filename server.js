f845aa6bde12f001331f27bcb7efda87583938ce
Graig Janssen <graig.janssen@gmail.com>
2018-09-14 22:26:21 -0400

    Server config

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/GalaxyDex'));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/GalaxyDex/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
